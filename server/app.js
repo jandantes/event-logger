579ebcf870583daae36726ca2435b9b036ea1306
Jan Dantes <paglubogngaraw@gmail.com>
2018-05-09 14:16:26 +0800

    added helmet and compression

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import session from 'express-session';
+import compression from 'compression';
 import mongoSessionStore from 'connect-mongo';
 import next from 'next';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
+
 import getRootUrl from '../lib/api/getRootUrl';
 import auth from './google';
 import api from './api';
@@ -11,7 +14,7 @@ import logger from './logs';
 require('dotenv').config();
 
 const dev = process.env.NODE_ENV !== 'production';
-const MONGO_URL = process.env.MONGO_URL_TEST;
+const MONGO_URL = dev ? process.env.MONGO_URL_TEST : process.env.MONGO_URL;
 
 mongoose.connect(MONGO_URL);
 
@@ -26,10 +29,22 @@ const handle = app.getRequestHandler();
 // Nextjs's server prepared
 app.prepare().then(() => {
   const server = express();
+
+  server.use(helmet());
+  server.use(compression());
   server.use(express.json());
 
   // potential fix for Error: Can't set headers
   // try with Chrome Dev Tools open/close
+
+  // pass all Nextjs's request to Nextjs server
+  server.get('/_next/*', (req, res) => {
+    handle(req, res);
+  });
+
+  server.get('/static/*', (req, res) => {
+    handle(req, res);
+  });
 
   // confuring MongoDB session store
   const MongoStore = mongoSessionStore(session);
@@ -47,6 +62,11 @@ app.prepare().then(() => {
       maxAge: 3 * 24 * 60 * 60 * 1000,
     },
   };
+
+  if (!dev) {
+    server.set('trust proxy', 1); // trust first proxy
+    sess.cookie.secure = true; // serve secure cookies
+  }
 
   server.use(session(sess));
 
